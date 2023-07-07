e509b936d0be0ca3853a50aaff24468eb957394c
Помойка <capulete@mail.ru>
2023-07-07 18:47:26 +0300

    add constructor in RepositoryOrderImp for initializing script file refactor read method

--- src/main/java/com/example/sqljdbc/repository/RepositoryOrderImp.java
+++ src/main/java/com/example/sqljdbc/repository/RepositoryOrderImp.java
@@ -1,7 +1,6 @@
 package com.example.sqljdbc.repository;
 
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -15,18 +14,25 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @Repository
-@RequiredArgsConstructor
 public class RepositoryOrderImp implements RepositoryOrder{
-    private static final String SCRIPT = "script1.sql";
+    private static final String scriptName = "script1.sql";
+    private final String script;
+
     private final NamedParameterJdbcTemplate parameterJdbcTemplate;
+
+    public RepositoryOrderImp(NamedParameterJdbcTemplate parameterJdbcTemplate) {
+        this.parameterJdbcTemplate = parameterJdbcTemplate;
+        script = read(scriptName);
+    }
 
     @Override
     public List<String> getProductName(String name) {
-        return parameterJdbcTemplate.queryForList(read(), Map.of("name", name), String.class);
+        return parameterJdbcTemplate.queryForList(read(script),
+                Map.of("name", name), String.class);
     }
 
-    private static String read() {
-        try (InputStream is = new ClassPathResource(SCRIPT).getInputStream();
+    private static String read(String scriptFileName) {
+        try (InputStream is = new ClassPathResource(scriptFileName).getInputStream();
              BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is))) {
             return bufferedReader.lines().collect(Collectors.joining("\n"));
         } catch (IOException e) {
